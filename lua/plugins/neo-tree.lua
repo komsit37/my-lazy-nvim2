245fb7c7e91bc47f90a15211738cdbdd1fc3575a
pkomsit <pkomsit@folio-sec.com>
2026-03-11 18:16:49 +0900

    add python support

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_by_name = {
+            "__init__.py",
+          },
+        },
+      },
+    },
+  },
+}
