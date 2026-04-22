ab6c13bd1bf22c8a1f1f5c48d932be27a1aece6e
pkomsit <pkomsit@folio-sec.com>
2026-04-22 11:21:43 +0900

    add neo tree sources

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,6 +2,22 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      sources = {
+        "filesystem",
+        "git_status",
+        "buffers",
+        "document_symbols",
+      },
+      source_selector = {
+        winbar = true,
+        statusline = false,
+        sources = {
+          { source = "filesystem", display_name = " 󰉓 Files " },
+          { source = "git_status", display_name = " 󰊢 Git " },
+          { source = "buffers", display_name = " 󰈚 Buffers " },
+          { source = "document_symbols", display_name = " 󰘦 Symbols " },
+        },
+      },
       filesystem = {
         filtered_items = {
           hide_by_name = {
