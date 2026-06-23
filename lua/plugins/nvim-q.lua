f02dd0e6eca9fdd9c4e304f1273b6958312cd18d
pkomsit <pkomsit@folio-sec.com>
2026-06-23 20:13:05 +0900

    add nvim-q

---
+++ lua/plugins/nvim-q.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    dir = "/home/pkomsit/work/lab/nvim-q",
+    name = "nvim-q",
+    ft = "q",
+    opts = {
+      connections = {
+        { name = "local", host = "localhost", port = 5555 },
+      },
+      keymaps = true,
+    },
+  },
+}
