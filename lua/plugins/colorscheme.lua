e406e2e1ee472997d43ed4859fe302eeed86f05e
pkomsit <pkomsit@folio-sec.com>
2026-07-09 12:24:12 +0900

    add color schemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,16 @@
 local hostname_fn = (vim.uv or vim.loop).os_gethostname
 local hostname = hostname_fn and hostname_fn() or ""
-local use_matteblack = hostname == "antec"
+local use_matteblack = hostname == "home-server"
 
 return {
-  { "rebelot/kanagawa.nvim", enabled = not use_matteblack },
-  { "tahayvr/matteblack.nvim", enabled = use_matteblack },
+  { "rebelot/kanagawa.nvim" },
+  { "tahayvr/matteblack.nvim" },
+  { "loctvl842/monokai-pro.nvim" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = use_matteblack and "matteblack" or "kanagawa",
+      colorscheme = use_matteblack and "matteblack" or "monokai-pro",
     },
   },
 }
